7a88a8d415d4ba3ec4c80ea5a770f8a93680617e
Ko, Byung Hyun <48151815+Prisoner737@users.noreply.github.com>
2019-11-29 20:31:06 +0900

    cards

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 from flask import Flask, jsonify, request
-from crypt import methods
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def main():
-    return "Hi~! This is test code of flash web server !! if you wannar get response Skill server, Go to /skill"
 
-@app.route("/test", methods=["GET"])
-def test():
-    data = jsonify(
-        version = 1.0,
-        value_list=[
-            "abc", "def"]
-        )
-    return data
-
-@app.route("/Skill", methods=["post","get"])
+@app.route("/skill", methods=["POST"])
 def skill():
+    print(request.json)
     data = {
-        "version" : "2.0",
-        "template" : {
+        "version": "2.0",
+        "template": {
             "outputs": [
                 {
-                    "simpleText" : {
-                        "text": "간단한 텍스트 요소입니다."
-                        }
+                    "basicCard":{
+                        "title": "카드의 제목",
+                        "description": "상세 설명",
+                        "thumbnail": {
+                            "imageUrl": "https://t1.daumcdn.net/friends/www/talk/kakaofriends_talk_2018.png"
+                            },
+                        "buttons": [
+                            {
+                                "label": "첫번째 버튼",
+                                "action": "message",
+                                "messageText": "첫번째 버튼을 눌렀습니다!"
+                            }
+                           
+                        ]
                     }
-                ]
-            }
-        }
+                }
+            ]
+        }   
+    }
+
     return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
